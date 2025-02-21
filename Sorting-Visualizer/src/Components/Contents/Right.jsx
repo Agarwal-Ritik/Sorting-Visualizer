855dd2fff2f5b457aad869bae785d5e2813c192d
shantanumamgain <shantanumamgain12@gmail.com>
2025-02-21 18:27:24 +0530

    Right part of the project, contains graph area, input area and graph css

---
+++ Sorting-Visualizer/src/Components/Contents/Right.jsx
@@ -0,0 +1,14 @@
+import './Right.css'
+import InputArea from './InputArea';
+import GraphArea from './GraphArea';
+
+function Right(){
+    return(
+        <div id="right">
+            <InputArea />
+            <GraphArea />
+        </div>
+    );
+}
+
+export default Right;
