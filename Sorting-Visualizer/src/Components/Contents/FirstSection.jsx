5e0e96d8101ed34948d16fb81b09a421986dd3d0
Agarwal-Ritik <agarwalritik80@gmail.com>
2025-02-21 18:14:23 +0530

    added the header footer and button react code files

---
+++ Sorting-Visualizer/src/Components/Contents/FirstSection.jsx
@@ -0,0 +1,14 @@
+import Introduction from "./Introduction";
+import GraphImage from "./GraphImage";
+import './FirstSection.css'
+
+function FirstSection() {
+  return (
+    <div id="first-section">
+      <Introduction />
+      <GraphImage />
+    </div>
+  );
+}
+
+export default FirstSection;
---
+++ Sorting-Visualizer/src/Components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import './header.css'
+function Header(){
+    return (
+        <header>
+            <h1>Sorting Visualizer</h1>
+            <button>Toggle Mode</button>
+        </header>
+    );
+}
+
+export default Header
