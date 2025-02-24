ecaa71324449c7c88bf88f40f6857e5fdc394278
Agarwal-Ritik <agarwalritik80@gmail.com>
2025-02-24 14:35:40 +0530

    Updated readme file and modified other files

--- Sorting-Visualizer/src/Components/Contents/RandomArray.js
+++ Sorting-Visualizer/src/Components/Contents/RandomArray.js
@@ -1,8 +1,11 @@
-
+const array = [];
 export function generateArray(arraysize) {
-  let array = [];
   for (let i = 0; i < arraysize; i++) {
     array.push(Math.floor(Math.random() * arraysize) + 1);
   }
+  return array;
+}
+
+export function returnArray(){
   return array;
 }
