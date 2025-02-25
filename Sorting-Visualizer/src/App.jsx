6cf64817bd0961ff063c46f0ddcd9faf3199ba60
Agarwal-Ritik <agarwalritik80@gmail.com>
2025-02-25 18:02:18 +0530

    removed some extras from input area

--- Sorting-Visualizer/src/App.jsx
+++ Sorting-Visualizer/src/App.jsx
@@ -3,7 +3,6 @@ import './App.css';
 import Header from './Components/Header/Header.jsx';
 import Foot from './Components/Footer/Foot.jsx';
 import Content from './Components/Contents/Content.jsx';
-import {DarkModeProvider} from './DarkModeContext.jsx';
 
 
 function App() {
