71d75f9d387464e20d78b2a894b806822f3136f7
Agarwal-Ritik <agarwalritik80@gmail.com>
2025-02-25 18:10:14 +0530

    minor change in app.jsx

--- Sorting-Visualizer/src/App.jsx
+++ Sorting-Visualizer/src/App.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import './App.css';
 import Header from './Components/Header/Header.jsx';
 import Foot from './Components/Footer/Foot.jsx';
 import Content from './Components/Contents/Content.jsx';
